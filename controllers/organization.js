953272e6115c4a6bd13982d042bd424a6f7cfd4e
Montini <devmontini@gmail.com>
2022-09-14 19:37:28 -0300

    Controller creado de organizations

--- controllers/organization.js
+++ controllers/organization.js
@@ -1,14 +1,1 @@
-const Organization = require("../models/organization");
-
-module.exports = class OrganizationController {
-  constructor() {}
-
-  findAllOrgs() {
-    try {
-      const organizations = new Organization.findAll();
-      return organizations;
-    } catch (error) {
-      return error;
-    }
-  }
-};
+module.exports = class OrganizationController {};
