7ff4e1eca1efdde04fcd3a759679092756e2107a
Montini <devmontini@gmail.com>
2022-09-14 12:15:34 -0300

    Se creo el  organization model, migration and seed

---
+++ seeders/20220914144706-create-organization.js
@@ -0,0 +1,42 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      "Organizations",
+      [
+        {
+          name: "Big Org",
+          image: "Lorem img BigOrg",
+          address: "Street one BigOrg",
+          phone: 123123123,
+          email: "bigorg@mail.com",
+          welcomeText: "Welcome to BigOrg",
+          aboutUsText: "About BigOrg",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Small Org",
+          image: "Lorem img SmallOrg",
+          address: null,
+          phone: null,
+          email: "smallorg@mail.com",
+          welcomeText: "Welcome to SmallOrg",
+          aboutUsText: null,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
