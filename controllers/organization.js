106e1dbd7ff3015c8d13016b7c011521178d351e
Tomas <tomasarreguib@gmail.com>
2022-09-15 15:38:19 -0300

    new: now organization controller brings public information from db

--- controllers/organization.js
+++ controllers/organization.js
@@ -1,3 +1,16 @@
-class OrganizationController {}
+const { Organization } = require('../models/');
+
+class OrganizationController {
+  async showPublicInformation() {
+    const findOrganization = await Organization.findOne();
+    return {
+      name: findOrganization.name,
+      image: findOrganization.image,
+      phone: findOrganization.phone,
+      address: findOrganization.address,
+    };
+  }
+}
 
 module.exports = OrganizationController;
+
