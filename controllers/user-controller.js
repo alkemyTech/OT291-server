e451a96388000a984af23d58aa7b0be8612e8af0
unknown <msosa89@outlook.com>
2022-09-16 10:27:21 -0300

    create post method on user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,3 +1,19 @@
-class UserController {}
+const db = require('../models');
+const { createUser } = require('../services/user');
+
+class UserController {
+  async post(req, res) {
+    try {
+      const user = await createUser(req.body);
+      res.send({
+        firstname: user.firstname,
+        lastname: user.lastname,
+        email: user.email,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
 
 module.exports = UserController;
