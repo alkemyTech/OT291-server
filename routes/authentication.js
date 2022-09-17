ad44567ee942b65c1c0840ac023b68bcb597bf5b
unknown <msosa89@outlook.com>
2022-09-17 13:22:04 -0300

    commit to pull

---
+++ routes/authentication.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var router = express.Router();
+const { post } = require('../controllers/user-controller');
+
+/* POST new user. */
+router.post('/register', post);
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const authRouter = require('./authentication')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.use('/auth', authRouter)
 
 module.exports = router;
