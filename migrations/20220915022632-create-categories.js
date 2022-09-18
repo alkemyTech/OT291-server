83cc4c100ce49c8766f590ceb2994851c475a348
sergio17 <sergiosalgado624@gmail.com>
2022-09-18 15:58:54 -0500

    Fields image and description migration nulleable

--- migrations/20220915022632-create-categories.js
+++ migrations/20220915022632-create-categories.js
@@ -18,7 +18,7 @@ module.exports = {
         type: Sequelize.STRING
       },
       image: {
-        allowNull: false,
+        allowNull: true,
         type: Sequelize.STRING
       },
       deletedAt: {
