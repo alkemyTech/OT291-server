70510358734a076732fa395c7d30be32d6d682c6
Tomas <tomasarreguib@gmail.com>
2022-09-19 14:09:07 -0300

    fix: all logic inside findOrganization method

--- controllers/organization.js
+++ controllers/organization.js
@@ -1,13 +1,15 @@
 const { Organization } = require('../models/');
 
 class OrganizationController {
-  static async showPublicInformation() {
+  static async findOrganization(req, res) {
     try {
-      const findOrganization = await Organization.findOne({
+      const response = await Organization.findOne({
         where: { name: 'Big Org' },
         attributes: ['name', 'image', 'phone', 'address'],
       });
-      return findOrganization;
+      response === null
+        ? res.status(404).json({ msg: 'Could not find information' })
+        : res.status(200).json(response);
     } catch (error) {
       return error;
     }
