20af1d2368ab3db7743f9069fdaa49dbabe2d281
Tomas <tomasarreguib@gmail.com>
2022-09-19 15:33:02 -0300

    fix: took off all inside router

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-const UserController = require('../controllers/user-controller');
-const userController = new UserController();
+const UserController = require('../controllers/user-controller.js');
+
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-router.delete('/:id', async (req, res, next) => {
-  const { id } = req.params;
-  try {
-    await userController.deleteUser(id);
-    res.status(200).json({ msg: 'User deleted successfully' });
-  } catch (error) {
-    next(error);
-  }
-});
+router.delete('/:id', UserController.deleteUser);
 
 module.exports = router;
