1ef93de7cbfc0348d12158291a111f32c3ee3134
Tomas <tomasarreguib@gmail.com>
2022-09-19 15:32:12 -0300

    fix: changed route name from deleteUser to user

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router();
 /* GET home page. */
 
-const deleteUser = require('./users.js');
+const user = require('./users.js');
 
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.use('/users', deleteUser);
+router.use('/users', user);
 
 module.exports = router;
