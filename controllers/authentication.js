be370ec48a904962585f1abb5f94e80e49a5387f
Montini <devmontini@gmail.com>
2022-09-21 01:20:49 -0300

    Se mejoro: - La logica y la respuesta del controller de authentication - La funcion _findUser de Dao ahora es abtracta y adaptable - Se agrego el rollback a create demo user

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -4,18 +4,20 @@ const AuthDao = require('../dao/authentication');
 
 class AuthController {
   static async loginUser(req, res) {
-    const { email } = req.body;
-    const { password } = req.body;
+    const { email, password } = req.body;
     try {
-      const userData = await AuthDao._findUser(email);
+      const objKeyValues = { email: email };
+      const arrayAttributes = ['email', 'password'];
+
+      const userData = await AuthDao._findUser(objKeyValues, arrayAttributes);
 
       if (!userData || !bcrypt.compareSync(password, userData.password)) {
         throw { ok: false };
       }
 
-      return res.status(200).json(userData);
+      res.status(200).json(userData);
     } catch (error) {
-      return res.status(400).json(error);
+      res.status(400).json(error);
     }
   }
 }
--- dao/authentication.js
+++ dao/authentication.js
@@ -1,19 +1,21 @@
 const db = require('../models');
 const { User } = db;
-
 class AuthDao {
-  static async _findUser(email) {
+  /**
+   * Asynchronously return a user from the database (table Users)
+   * @param {Object} objKeyValues - To filter by attribute/s Ej: {email: 'user@mail'}
+   * @param {string[]} arrayAttributes - Attributes we need Ej: ['username', 'age']
+   */
+  static async _findUser(objKeyValues, arrayAttributes) {
     try {
       const userData = await User.findOne({
-        where: {
-          email: email,
-        },
-        attributes: ['email', 'password'],
+        where: objKeyValues,
+        attributes: arrayAttributes,
       });
 
       return userData;
     } catch (error) {
-      return res.status(400).json(error);
+      res.status(400).json(error);
     }
   }
 }
