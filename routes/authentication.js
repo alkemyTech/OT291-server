0017b5ab88452b7e57164f86d8455a42aa7c737e
Montini <devmontini@gmail.com>
2022-09-21 21:37:30 -0300

    Se actualizo rama local con development y se arreglaron los conflictos

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,9 +1,18 @@
-const express = require('express');
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
+const userSchema = require('../schemas/user');
 const { loginValidation } = require('../schemas/login');
-
+const ValidationErrors = require('../middlewares/validationErrors');
+const UserController = require('../controllers/user-controller');
 const AuthController = require('../controllers/authentication.js');
 
 router.post('/login', loginValidation, AuthController.loginUser);
+
+router.post(
+  '/register',
+  userSchema,
+  ValidationErrors.validateSchema,
+  UserController.post
+);
 
 module.exports = router;
