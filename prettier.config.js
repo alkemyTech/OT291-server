8df8996a2ae216f2b97006a7b6e8efaee406974b
Ricardo Ramos <freeazul25@outlook.es>
2022-09-22 13:26:21 -0500

    aws-sdk added to config, aws.config updated, s3Bucket created

--- prettier.config.js
+++ prettier.config.js
@@ -1,12 +1,11 @@
-const options = {
-  arrowParents: 'avoid',
-  singleQuote: true,
-  backetSpacing: true,
-  endOfLine: 'lf',
-  semi: true,
-  tabWidth: 2,
-  traillingComma: 'none',
-};
+// const options = {
+//   arrowParents: 'avoid',
+//   singleQuote: true,
+//   backetSpacing: true,
+//   endOfLine: 'lf',
+//   semi: true,
+//   tabWidth: 2,
+//   traillingComma: 'none',
+// };
 
 module.exports = options;
-
