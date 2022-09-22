b7e594f338385ec820488493ab148f2cb9ac3472
unknown <msosa89@outlook.com>
2022-09-22 12:59:29 -0300

    Create post method

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,3 +1,24 @@
-class CategoriesController {}
+const { Category } = require('../models');
+
+class CategoriesController {
+  static async post(req, res, next) {
+    try {
+      const { name, description, image } = req.body;
+      const category = await Category.create({
+        name,
+        description,
+        image,
+      });
+      const response = {
+        name: category.name,
+        description: category.description,
+        image: category.image,
+      };
+      res.json(response);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
 
 module.exports = CategoriesController;
