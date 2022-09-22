b81088fbeb20d666fc1ee8b671a1dc2d7bc876ba
Ricardo Ramos <freeazul25@outlook.es>
2022-09-22 15:50:14 -0500

    organizations schema added, organization update endpoint added

--- controllers/organization.js
+++ controllers/organization.js
@@ -14,6 +14,38 @@ class OrganizationController {
       return error;
     }
   }
+
+  static async updateOrganization(req, res) {
+    try {
+      const { name, image, phone, address, email, welcomeText, aboutUsText } =
+        req.body;
+      const organization = await Organization.findByPk(1);
+
+      if (!organization) {
+        return res.status(404).json({
+          msg: 'There is no organization',
+        });
+      }
+
+      await organization.update({
+        name,
+        image,
+        phone,
+        address,
+        email,
+        welcomeText,
+        aboutUsText,
+      });
+
+      return res.json({
+        msg: `Organization info has been updated`,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        msg: 'Error in db',
+      });
+    }
+  }
 }
 
 module.exports = OrganizationController;
