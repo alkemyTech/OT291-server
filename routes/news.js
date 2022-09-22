2a1df8377b9bd16b41f5ee1d6f65c26d43c145a3
alvaro1599 <alvarocanales1599@gmail.com>
2022-09-22 06:42:20 -0500

    Added Role Middleware

--- routes/news.js
+++ routes/news.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const NewsController = require('../controllers/news.js');
 const VerifyType = require("../middlewares/verifyType")
 const VerifyRole = require("../middlewares/verify-role")
-router.get('/:id',VerifyType.VerifyId,NewsController.findById);
+router.get('/:id',VerifyRole.isAdminRole,VerifyType.VerifyId,NewsController.findById);
 
 module.exports = router;
