0528ab4355f930cf86ae3cd59af54dd6d523116b
unknown <msosa89@outlook.com>
2022-09-23 16:34:26 -0300

    Add validation on description and image field

--- schemas/category.js
+++ schemas/category.js
@@ -7,6 +7,18 @@ const categorySchema = [
     .withMessage('name has to be a string')
     .notEmpty()
     .withMessage('name is required'),
+  body('description')
+    .isString()
+    .trim()
+    .withMessage('description has to be a string')
+    .notEmpty()
+    .withMessage('description is required'),
+  body('image')
+    .isString()
+    .trim()
+    .withMessage('image has to be a string')
+    .notEmpty()
+    .withMessage('image is required'),
 ];
 
 module.exports = categorySchema;
