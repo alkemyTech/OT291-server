3983cb00c47da2fca50ebc51d70e525c77cd60b8
sergio17 <sergiosalgado624@gmail.com>
2022-09-26 13:20:38 -0500

    end point to update news - fixing comments

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,9 @@ const authRouter = require('./authentication');
 const categories = require('./categories');
 const organization = require('./organization.js');
 const user = require('./users.js');
-<<<<<<< HEAD
 const activities = require('./activities.js');
-=======
 const news = require('./news')
->>>>>>> a199dd3 ( endpoint to put news ok)
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -18,7 +16,8 @@ router.get('/', function (req, res, next) {
 router.use('/auth', authRouter);
 router.use('/organization', organization);
 router.use('/users', user);
-router.use('/categories', category);
-router.use('/news/', news )
+router.use('/activities', activities);
+router.use('/categories', categories);
+router.use('/news/', news);
 
 module.exports = router;
