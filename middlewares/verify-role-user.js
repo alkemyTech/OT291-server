e762ebcb28d4e141a95ae2c42259ac427e42ade6
Tomas <tomasarreguib@gmail.com>
2022-09-27 02:12:56 -0300

    feat: decryptTokenUser returns the user only when it has provided a valid token. Improvements in the code

--- middlewares/verify-role-user.js
+++ middlewares/verify-role-user.js
@@ -1,35 +1,31 @@
-const { Token } = require('../helpers/token');
+const Token = require('../helpers/Token');
 const { User, Role } = require('../models');
 
 class ValidatejWtUser {
   static async decryptTokenUser(req, res, next) {
     const decryptToken = Token.decryptJWT(req, res);
 
-    if ( !decryptToken || !decryptToken.email ) {
+    if (!decryptToken || !decryptToken.email) {
       return res.status(401).json('jwt must be provided or invalid');
     }
-
+    const { email } = decryptToken;
     try {
       const user = await User.findOne({
-        where: {
-          email: decryptToken.email,
-        },
-        attributes: ['firstName'],
+        where: { email },
+        attributes: ['firstName', 'lastName', 'email', 'image'],
         include: {
           model: Role,
           as: 'role',
           attributes: ['name'],
         },
       });
-
       if (!user) {
         return res.status(404).json({ msg: 'Could not find user' });
       }
-
       if (user.role.name !== 'Standard') {
         return res.status(403).json({ msg: 'unauthorized user' });
       }
-
+      req.user = user;
       next();
     } catch (error) {
       res.status(404).json({ msg: 'db response error' });
@@ -37,6 +33,4 @@ class ValidatejWtUser {
   }
 }
 
-module.exports = {
-  ValidatejWtUser,
-};
+module.exports = ValidatejWtUser;
