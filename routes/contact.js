130b97879bc63a8d5106beb80f28065a065bb606
alvaro1599 <alvarocanales1599@gmail.com>
2022-09-26 20:50:51 -0500

    se eliminó ruta routes/contact

--- routes/contact.js
+++ routes/contact.js
@@ -2,8 +2,4 @@ const express = require('express');
 const {Contact} = require("../models")
 const router = express.Router();
 
-router.get("/",(req,res)=>{
-    res.send("Esta es la ruta de contactos")
-})
-
 module.exports = router;
