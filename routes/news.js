908f0380bda3b43e4e52669388c6c9cdeb9978d6
sergio17 <sergiosalgado624@gmail.com>
2022-09-26 20:54:43 -0500

    endpoint update news using Dao

--- routes/news.js
+++ routes/news.js
@@ -3,8 +3,21 @@ const router = express.Router();
 const News = require('../controllers/news');
 const ValidationErrors = require('../middlewares/validationErrors');
 const RoleMiddleware = require('../middlewares/verify-role');
-const {getByIdSchema} = require('../schemas/news');
+const { getByIdSchema, putNewsSchema } = require('../schemas/news');
 
-router.get("/:id",RoleMiddleware.isAdminRole,getByIdSchema,ValidationErrors.validateSchema,News.DetailNew)
+router.get(
+  '/:id',
+  RoleMiddleware.isAdminRole,
+  getByIdSchema,
+  ValidationErrors.validateSchema,
+  News.DetailNew
+);
+router.put(
+  '/:id',
+  RoleMiddleware.isAdminRole,
+  putNewsSchema,
+  ValidationErrors.validateSchema,
+  News.putNew
+);
 
 module.exports = router;
