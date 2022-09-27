c7b5cc174dd1ac82b2f420fff6f05045150ac78b
alvaro1599 <alvarocanales1599@gmail.com>
2022-09-26 20:50:09 -0500

    se eliminaron comentarios models/contact

--- models/contact.js
+++ models/contact.js
@@ -4,30 +4,29 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Contact extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
     }
   };
   Contact.init({
     name: {
-      allowNull:false,
-      type:DataTypes.STRING},
+      allowNull: false,
+      type: DataTypes.STRING
+    },
     phone: {
-      allowNull:false,
-      type:DataTypes.INTEGER},
+      allowNull: false,
+      type: DataTypes.INTEGER
+    },
     email: {
-      allowNull:false,
-      type:DataTypes.STRING},
+      allowNull: false,
+      type: DataTypes.STRING
+    },
     message: {
-      allowNull:false,
-      type:DataTypes.STRING},
+      allowNull: false,
+      type: DataTypes.STRING
+    },
     deletedAt: {
-      type:DataTypes.DATE}
+      type: DataTypes.DATE
+    }
   }, {
     sequelize,
     modelName: 'Contact',
