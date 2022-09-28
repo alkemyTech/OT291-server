b7d005a8663fb6bb139c631bbd0030c41f522206
Tomas <tomasarreguib@gmail.com>
2022-09-28 16:06:06 -0300

    modificacion de la ruta

--- routes/authentication.js
+++ routes/authentication.js
@@ -20,6 +20,6 @@ router.post(
   UserController.post
 );
 
-router.get('/me', UserController.getData)
+router.get('/me', ValidatejWtUser.decodedToken, UserController.getData)
 
 module.exports = router;
