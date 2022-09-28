7ba90205c6601104f99f9935b4bf30e27e25ba42
rich27a <freeazul25@outlook.es>
2022-09-27 19:44:45 -0500

    fix merge ot51 delete new

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,22 @@
 var express = require('express');
 var router = express.Router();
 const authRouter = require('./authentication');
-<<<<<<< HEAD
-const news = require('./news');
-=======
 const categories = require('./categories');
->>>>>>> 90a3586d05669d88ef488d7f2e4b3c5dde98af8a
 const organization = require('./organization.js');
 const user = require('./users.js');
 const news = require('./news');
 const activities = require('./activities.js');
-const contact = require("./contact")
+const contact = require('./contact');
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.use('/auth', authRouter);
-<<<<<<< HEAD
-router.use('/news', news);
-=======
->>>>>>> 90a3586d05669d88ef488d7f2e4b3c5dde98af8a
 router.use('/organization', organization);
 router.use('/users', user);
 router.use('/activities', activities);
 router.use('/categories', categories);
-router.use('/contacts',contact);
-router.use('/news', news );
+router.use('/contacts', contact);
+router.use('/news', news);
 module.exports = router;
