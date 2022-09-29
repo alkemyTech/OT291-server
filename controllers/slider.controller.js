9a5c5ebed8201fb9e3dc3b1ec3b624f9793391fe
Tomas <tomasarreguib@gmail.com>
2022-09-29 18:49:05 -0300

    new: method to create a new slide

--- controllers/slider.controller.js
+++ controllers/slider.controller.js
@@ -1,4 +1,5 @@
 const SlidesDao = require('../dao/slide');
+const { Slide } = require('../models');
 class SlidersController {
   static async getDetails(req, res) {
     const { id } = req.params;
@@ -16,6 +17,34 @@ class SlidersController {
       }
     } catch (error) {
       res.status(400).json(error);
+    }
+  }
+  static async postSlide(req, res) {
+    const { imageUrl, text } = req.body;
+    let { order } = req.body;
+
+    let allSlides;
+    let slidesOrder;
+
+    try {
+      allSlides = await Slide.findAll();
+      slidesOrder = allSlides
+        .map(e => e.toJSON())
+        .map(o => o.order)
+    } catch(error) {
+      res.status(400).json({ msg: 'Could not find slides' })
+    }
+
+    try {
+    if (!order) order = slidesOrder.length + 1
+    const newSlide = await SlidesDao.createSlide({
+      imageUrl,
+      text,
+      order,
+    }, { fields: ['imageUrl', 'text', 'order'] })
+      res.status(200).json(newSlide)
+    } catch (error) {
+      res.status(400).json({ msg: 'Could not create a Slide' })
     }
   }
 }
