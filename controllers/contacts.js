6ff8e69ba8709f02f135d2535ec91f7fd8c2b52c
rich27a <freeazul25@outlook.es>
2022-09-29 17:59:32 -0500

    contacts/backoffice/ route added, getContacts added to ContactController

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,15 +1,44 @@
-const ContactDao = require("../dao/contact")
+const { request, response } = require('express');
+const ContactDao = require('../dao/contact');
+const { Contact } = require('../models/');
 class ContactController {
-    static async createContact(req, res) {
-        try {
-            const { name, email, phone, message } = req.body
-            const contactData = await ContactDao.createContact({ name, email, phone: phone ?? null, message: message ?? null })
-            return res.status(201).json(contactData)
-        } catch (error) {
-            return res.status(500).json(error)
-        }
-
+  static async createContact(req, res) {
+    try {
+      const { name, email, phone, message } = req.body;
+      const contactData = await ContactDao.createContact({
+        name,
+        email,
+        phone: phone ?? null,
+        message: message ?? null,
+      });
+      return res.status(201).json(contactData);
+    } catch (error) {
+      return res.status(500).json(error);
     }
+  }
+
+  static async getContacts(req = request, res = response) {
+    try {
+      const contacts = await Contact.findAll({
+        attributes: ['name', 'phone', 'email', 'message', 'createdAt'],
+      });
+
+      if (!contacts) {
+        return res.status(404).json({
+          msg: 'There is no contacts registered',
+        });
+      }
+
+      return res.status(200).json({
+        contacts,
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        msg: 'Error while searching in db',
+      });
+    }
+  }
 }
 
-module.exports = ContactController
+module.exports = ContactController;
--- routes/contact.js
+++ routes/contact.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const ContactController=require("../controllers/contacts")
+const ContactController = require('../controllers/contacts');
 const ValidationErrors = require('../middlewares/validationErrors');
-const {createContactSchema} = require('../schemas/contact');
+const { createContactSchema } = require('../schemas/contact');
+const verifyRole = require('../middlewares/verify-role');
 
-router.post("/",createContactSchema,ValidationErrors.validateSchema,ContactController.createContact)
+router.post(
+  '/',
+  createContactSchema,
+  ValidationErrors.validateSchema,
+  ContactController.createContact
+);
+
+router.get(
+  '/backoffice',
+  verifyRole.isAdminRole,
+  ContactController.getContacts
+);
 
 module.exports = router;
