4ae656a2c808acfde65808c1e827095d546525d6
sergio17 <sergiosalgado624@gmail.com>
2022-09-29 17:19:33 -0500

    add socialm media fields to endpoint public of organizations

--- controllers/organization.js
+++ controllers/organization.js
@@ -5,13 +5,15 @@ class OrganizationController {
     try {
       const response = await Organization.findOne({
         where: { name: 'Big Org' },
-        attributes: ['name', 'image', 'phone', 'address'],
+        attributes: ['name', 'image', 'phone', 'address', 'urlFacebook', 'urlInstagram', 'urlLinkedin'],
       });
       response === null
         ? res.status(404).json({ msg: 'Could not find information' })
         : res.status(200).json(response);
     } catch (error) {
-      return error;
+      return res.status(500).json({
+        msg: 'Error in db',
+      });
     }
   }
 
