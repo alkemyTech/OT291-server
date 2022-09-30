a549508acc9e61bf30d779e50ad02f4553a203b3
Tomas <tomasarreguib@gmail.com>
2022-09-29 23:07:53 -0300

    deleted orderSlides method

--- helpers/Slide.js
+++ helpers/Slide.js
@@ -1,18 +1,6 @@
 const { Slide } = require('../models');
 
 class SlideHelper {
-  static async orderSlides(order) {
-    if (order) return order;
-
-    try {
-      const allSlides = await Slide.findAll();
-      const slidesOrder = allSlides.map((e) => e.toJSON()).map((o) => o.order);
-      order = slidesOrder.length + 1;
-      return order;
-    } catch (error) {
-      return error;
-    }
-  }
   static decodeImage(imageUrl) {
     try {
       const asciiToBinary = Buffer.from(imageUrl, 'base64').toString('binary');
