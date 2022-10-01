4b1a09604aac03780613e50af9be93649afe25b4
Tomas <tomasarreguib@gmail.com>
2022-09-30 22:52:38 -0300

    new: route to get all contacts as administrator

--- routes/contact.js
+++ routes/contact.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const ContactController = require('../controllers/contacts');
 const ValidationErrors = require('../middlewares/validationErrors');
+const RoleMiddleware = require('../middlewares/verify-role');
 const { createContactSchema } = require('../schemas/contact');
 
 router.post(
@@ -9,6 +10,12 @@ router.post(
   createContactSchema,
   ValidationErrors.validateSchema,
   ContactController.createContact
+);
+
+router.get(
+  '/contacts',
+  RoleMiddleware.isAdminRole,
+  ContactController.getContacts
 );
 
 module.exports = router;
