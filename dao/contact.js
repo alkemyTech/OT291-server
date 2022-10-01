1905d2e1702887e03aadc6857565b2fa260ee989
Tomas <tomasarreguib@gmail.com>
2022-09-30 22:23:51 -0300

    refactor: se aplico prettier a lo que ya estaba

--- dao/contact.js
+++ dao/contact.js
@@ -1,15 +1,16 @@
-const MapperResponse = require("../utils/formatResponse")
-const { Contact } = require("../models/")
+const MapperResponse = require('../utils/formatResponse');
+const { Contact } = require('../models/');
+
 class ContactDao {
-    static async createContact(body) {
-        try {
-            const contactData = await Contact.create( body )
-            const result=MapperResponse.cleanDataDb(contactData)
-            return result
-        } catch (error) {
-            throw new Error(error)
-        }
+  static async createContact(body) {
+    try {
+      const contactData = await Contact.create(body);
+      const result = MapperResponse.cleanDataDb(contactData);
+      return result;
+    } catch (error) {
+      throw new Error(error);
     }
+  }
 }
 
-module.exports = ContactDao
+module.exports = ContactDao;
