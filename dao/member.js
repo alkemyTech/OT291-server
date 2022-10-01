da902755d5e09ae8596f20f251c97be2dd2b59d8
Johannes Gómez <87533958+JohannesGomez@users.noreply.github.com>
2022-10-01 18:35:40 -0300

    Endpoint Removal Members

--- dao/member.js
+++ dao/member.js
@@ -9,7 +9,7 @@ class MemberDao {
   static async deleteMember(where) { 
      try {
           let deletedMember = await Member.destroy({
-          where: where,
+          where,
         });
         return deletedMember;
      } catch (error) {
