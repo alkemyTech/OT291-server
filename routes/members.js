f38056b83397d2314de662611694693287895c66
unknown <msosa89@outlook.com>
2022-10-03 14:57:07 -0300

    Added route.

--- routes/members.js
+++ routes/members.js
@@ -2,14 +2,22 @@ const express = require('express');
 const router = express.Router();
 const RoleMiddleware = require('../middlewares/verify-role');
 const MemberController = require('../controllers/member.controller');
-const membersByIdSchema = require('../schemas/members');
+const { membersByIdSchema, newMemberSchema } = require('../schemas/members');
 const ValidationErrors = require('../middlewares/validationErrors');
 
-router.delete('/:id', 
-    membersByIdSchema,
-    ValidationErrors.validateSchema,
-    RoleMiddleware.isAdminRole, 
-    MemberController.deleteMember
-)
+router.delete(
+  '/:id',
+  membersByIdSchema,
+  ValidationErrors.validateSchema,
+  RoleMiddleware.isAdminRole,
+  MemberController.deleteMember
+);
+
+router.post(
+  '/',
+  newMemberSchema,
+  ValidationErrors.validateSchema,
+  MemberController.postNewMember
+);
 
 module.exports = router;
