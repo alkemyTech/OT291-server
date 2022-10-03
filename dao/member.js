e0518af59f00f60b47dc169c56b691d9d0979e5c
unknown <msosa89@outlook.com>
2022-10-03 14:56:11 -0300

    Added postNewMember in DAO.

--- dao/member.js
+++ dao/member.js
@@ -1,21 +1,43 @@
-const {Member} = require('../models');
+const { Member } = require('../models');
 
 class MemberDao {
   /**
-  * Asynchronously delete a member from the database (table Members)
-  * @param   {Object} where - To filter by attribute/s Ej: {id: '1'}
-  * @returns {boolean}
-  */
-   static async deleteMember(where) { 
-     try {
-            let deletedMember = await Member.destroy({
-               where,
-            });
-           return deletedMember;
-         } catch (error) {
-         throw error;
-      }
-   }
+   * Asynchronously delete a member from the database (table Members)
+   * @param   {Object} where - To filter by attribute/s Ej: {id: '1'}
+   * @returns {boolean}
+   */
+  static async deleteMember(where) {
+    try {
+      let deletedMember = await Member.destroy({
+        where,
+      });
+      return deletedMember;
+    } catch (error) {
+      throw error;
+    }
+  }
+  static async postNewMember({
+    name,
+    facebookUrl,
+    instagramUrl,
+    linkedinUrl,
+    image,
+    description,
+  }) {
+    try {
+      await Member.create({
+        name,
+        facebookUrl,
+        instagramUrl,
+        linkedinUrl,
+        image,
+        description,
+      });
+      return;
+    } catch (error) {
+      throw new Error('Error. Member not created.');
+    }
+  }
 }
 
 module.exports = MemberDao;
