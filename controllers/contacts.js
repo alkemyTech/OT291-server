60eb399a72012963e041aeab1ad0db96412b2114
alvaro1599 <alvarocanales1599@gmail.com>
2022-10-03 00:15:53 -0500

    Se añadió el envío de email de contacto

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,9 +1,11 @@
 const ContactDao = require("../dao/contact")
+const NotifyViaEmail = require("../services/notifyViaEmail")
 class ContactController {
     static async createContact(req, res) {
         try {
             const { name, email, phone, message } = req.body
             const contactData = await ContactDao.createContact({ name, email, phone: phone ?? null, message: message ?? null })
+            await NotifyViaEmail.sendEmail(email,"Hemos registrado tus datos con éxito",`Hola ${name} te agradecemos por registrar tus datos, pronto nos pondremos en contacto contigo.`)
             return res.status(201).json(contactData)
         } catch (error) {
             return res.status(500).json(error)
