d37de6623d3653b0fbc93a74eac0f57ebd5fef67
unknown <msosa89@outlook.com>
2022-10-04 16:40:25 -0300

    fix import members schemas

--- routes/members.js
+++ routes/members.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const MemberController = require('../controllers/member.controller');
 const RoleMiddleware = require('../middlewares/verify-role');
-const membersByIdSchema = require('../schemas/members');
+const { membersByIdSchema, newMemberSchema } = require('../schemas/members');
 const ValidationErrors = require('../middlewares/validationErrors');
 
 router.get('/', RoleMiddleware.isAdminRole, MemberController.getMembers);
