464185840764e35d4d8223a60b9e97047d13fe8a
Johannes Gómez <87533958+JohannesGomez@users.noreply.github.com>
2022-10-04 20:24:12 -0300

    Endpoint to list Comments

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -4,7 +4,7 @@ const CommentsDao = require('../dao/comments')
 class CommentsController {
     static async getAllComments(req, res)  {
         const attributes = ['body'];
-        const order = [['createdAt','ASC']];
+        const order = [['createdAt','DESC']];
         let getAllComments;
         try {
             getAllComments = await CommentsDao.getAllComments(attributes, order);
