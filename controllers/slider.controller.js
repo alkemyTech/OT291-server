dc34d90e63df5795e47bd03a31713c1ff05c229f
alvaro1599 <alvarocanales1599@gmail.com>
2022-10-13 14:46:53 -0500

    se añadió la subida de imagenes

--- controllers/slider.controller.js
+++ controllers/slider.controller.js
@@ -35,27 +35,36 @@ class SlidersController {
     }
   }
 
-  static async updateSlide(req,res){
-    const {id} = req.params;
-    const {imageUrl,text,order,organizationId} = req.body;
+  static async updateSlide(req, res) {
+    const { id } = req.params;
+    const { imageUrl, text, order, organizationId } = req.body;
+    let decodedImage;
     try {
-      const slideUpdated=await SlidesDao.updateSLide({imageUrl,text,order,organizationId},+id)
-      return res.status(200).json(slideUpdated)
+      decodedImage = await UploadFiles.decodeImage(imageUrl, id);
     } catch (error) {
-      res.status(500).send(error.message)
+      res.status(400).json({ msg: 'Could not decode image' });
+    }
+    try {
+      const slideUpdated = await SlidesDao.updateSLide(
+        { imageUrl: decodedImage, text, order, organizationId },
+        +id
+      );
+      return res.status(200).json(slideUpdated);
+    } catch (error) {
+      res.status(500).send(error.message);
     }
   }
 
   static async deleteSlide(req, res) {
     const { id } = req.params;
-    const where = { id } ;
-    
+    const where = { id };
+
     try {
       const response = await SlidesDao.deleteSlides(where);
-      
-      response ? res.status(200).json({ msg: 'Slide deleted successfully' })
-      : res.status(404).json({ msg: 'Could not find Slide' })
 
+      response
+        ? res.status(200).json({ msg: 'Slide deleted successfully' })
+        : res.status(404).json({ msg: 'Could not find Slide' });
     } catch (error) {
       return res.status(500).json({
         error,
@@ -69,18 +78,21 @@ class SlidersController {
     let { order } = req.body;
     let decodedImage;
 
-    if (!name) return res.status(400).json({
-      msg: 'A name is required',
-    });
+    if (!name)
+      return res.status(400).json({
+        msg: 'A name is required',
+      });
 
     try {
-      decodedImage = await UploadFiles.decodeImage(base64Image, name)
-    } catch(error) {
-      res.status(400).json({ msg: 'Could not decode image' })
+      decodedImage = await UploadFiles.decodeImage(base64Image, name);
+    } catch (error) {
+      res.status(400).json({ msg: 'Could not decode image' });
     }
 
     if (!order) {
-      order = (await SlidesDao.sortSlides('order', 'DESC')).map(o => o.order)[0] + 1
+      order =
+        (await SlidesDao.sortSlides('order', 'DESC')).map((o) => o.order)[0] +
+        1;
     }
 
     try {
@@ -90,11 +102,11 @@ class SlidersController {
           order,
           text,
         },
-        [ 'imageUrl', 'order', 'text' ]
+        ['imageUrl', 'order', 'text']
       );
-      res.status(200).json({ msg: 'Slide created successfully' })
+      res.status(200).json({ msg: 'Slide created successfully' });
     } catch (error) {
-      res.status(400).json({ msg: 'Could not create a slide' })
+      res.status(400).json({ msg: 'Could not create a slide' });
     }
   }
 }
