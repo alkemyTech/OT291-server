d01791709cf9c14a4b1f23d1791f156d2c2b455d
Montini <devmontini@gmail.com>
2022-10-18 15:16:35 -0300

    Se agrego el MVC de Orders para MP

---
+++ migrations/20221018180430-create-orders.js
@@ -0,0 +1,46 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Orders', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      category: {
+        type: Sequelize.ENUM('singleDonation', 'recurrentDonation'),
+      },
+      amount: {
+        type: Sequelize.INTEGER,
+      },
+      userEmail: {
+        type: Sequelize.STRING,
+      },
+      paymentLink: {
+        type: Sequelize.STRING,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Orders');
+  },
+};
