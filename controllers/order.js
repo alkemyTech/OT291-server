4819d0bd8b90ef105e9cda1e517536e43ef406ec
unknown <msosa89@outlook.com>
2022-10-19 13:22:37 -0300

    fix endpoint orders

--- controllers/order.js
+++ controllers/order.js
@@ -7,23 +7,37 @@ const UserDao = require('../dao/user');
 class OrderController {
   static PaymentCategory = PaymentConstants.category;
   static async createOrder(req, res) {
+    let paymentLink;
     const { category, amount, userEmail } = req.body;
     const data = await Token.decryptJWT(req);
-    console.log(data);
     if (category === OrderController.PaymentCategory.recurrent) {
       try {
-        const paymentLink = await PaymentService.createSubscription(amount);
-        const user = await UserDao.findOneUser({ email: data.email });
-        await OrderDao.createOrder({
-          category,
+        paymentLink = await PaymentService.createSubscription(
           amount,
-          paymentLink,
-          user_id: user.id,
-        });
-        res.status(200).json(paymentLink);
+          'ARS',
+          userEmail
+        );
       } catch (error) {
         res.status(402).send(error.message);
       }
+    } else if (category === OrderController.PaymentCategory.single) {
+      try {
+        paymentLink = await PaymentService.createPayment(userEmail, amount);
+      } catch (error) {
+        res.status(402).send(error.message);
+      }
+    }
+    try {
+      const user = await UserDao.findOneUser({ email: data.email });
+      await OrderDao.createOrder({
+        category,
+        amount,
+        paymentLink,
+        user_id: user.id,
+      });
+      res.status(200).json(paymentLink);
+    } catch (error) {
+      res.status(402).send(error.message);
     }
   }
 }
--- services/paymentService.js
+++ services/paymentService.js
@@ -1,5 +1,6 @@
 const PaymentConstants = require('../constants/payment');
 const HttpAdapter = require('../services/HttpAdapter');
+const axios = require('axios');
 
 class PaymentService {
   static Url = PaymentConstants.MercadoPago;
@@ -11,18 +12,23 @@ class PaymentService {
       transaction_amount: null,
     },
     back_url: 'https://google.com.ar',
-    payer_email: `${process.env.EMAIL_MERCADO_PAGO}`,
+    payer_email: '',
   };
 
-  static setBodySubscription(amount, rate_change) {
+  static setBodySubscription(amount, rate_change, userEmail) {
     PaymentService.bodySubscription.auto_recurring.transaction_amount = amount;
     PaymentService.bodySubscription.auto_recurring.currency_id = rate_change;
+    PaymentService.bodySubscription.payer_email = userEmail;
     return PaymentService.bodySubscription;
   }
 
-  static async createSubscription(amount, rate_change = 'ARS') {
+  static async createSubscription(amount, rate_change = 'ARS', userEmail) {
     try {
-      const body = PaymentService.setBodySubscription(amount, rate_change);
+      const body = PaymentService.setBodySubscription(
+        amount,
+        rate_change,
+        userEmail
+      );
       const payment = await HttpAdapter.post(
         PaymentService.Url,
         body,
@@ -31,6 +37,39 @@ class PaymentService {
       return payment.init_point;
     } catch (error) {
       throw new Error(error);
+    }
+  }
+
+  static async createPayment(email, ammount) {
+    try {
+      const url = 'https://api.mercadopago.com/checkout/preferences';
+      const body = {
+        payer_email: email,
+        items: [
+          {
+            title: 'Donación única',
+            description: 'Gracias por tu donación.',
+            picture_url: 'https://placehold.co/32',
+            category_id: 'singleDonation',
+            quantity: 1,
+            unit_price: ammount,
+          },
+        ],
+        back_urls: {
+          failure: '/failure',
+          pending: '/pending',
+          success: '/success',
+        },
+      };
+      const payment = await axios.post(url, body, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${process.env.ACCESS_TOKEN_MERCADO_PAGO}`,
+        },
+      });
+      return payment.data.init_point;
+    } catch (error) {
+      throw error;
     }
   }
 }
